f54200e08188640263f540e44fea8a61bfbf0d8a
Addisalem <addisalemdes@gmail.com>
2021-03-28 21:18:22 -0500

    w1d6Assignment

--- CS303/labW1D6/arrayPractice.js
+++ CS303/labW1D6/arrayPractice.js
@@ -1,11 +1,12 @@
 
 
 
-"use strict"
+// "use strict"
 /* eslint-disable */
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
-// module.exports = { doubleNums, doubleAges, filterEven, filterOver10, findEvenNum, findEvenAge, includesEvenNum, includesEvenAge }; //add all of your function names here that you need for the node mocha tests
-
+// module.exports = { doubleNums, doubleAges, filterEven, filterOver10, findEvenNum, findEvenAge, includesEvenNum, includesEvenAge, sumOfNumbers, averageOfNumbers, maxOfNumbers, maxOfAges, avgAgeEvenNum, avgAgeoddNum };
+//add all of your function names here that you need for the node mocha tests
+// averageOfNumbers, maxOfNumbers, maxOfAges, avgAgeEvenNum, avgAgeoddNum,includesEvenAge
 
 const numArray = [5, 0, 7, 77, -20, 300, 51, 2]
 const peopleArray = [{ name: "Sam", age: 15 }, { name: "William", age: 6 }, { name: "Lucy", age: 13 }, {
@@ -18,18 +19,10 @@ function doubleNums(arr) {
 // console.log(doubleNums(numArray))
 
 function doubleAges(arr) {
-    // for (let key in arr) {
-    //     if (key === "age") {
-    //         arr = arr[key] *= 2
-    //     }
-
-    // }
-
-    // return arr
+    return arr.map(age => `${age.age * 2}`)
 }
-// const doub = peopleArray.map(doubleAges)
-// doub()
-// // console.log(doub)
+
+
 
 
 function filterEven(arr) {
@@ -41,7 +34,7 @@ function filterEven(arr) {
 function filterOver10(arr) {
 
 
-    return arr.filter(x => x > 10)
+    return arr.filter(arr => arr.age > 10)
 
 }
 function findEvenNum(arr) {
@@ -50,16 +43,74 @@ function findEvenNum(arr) {
 }
 
 function findEvenAge(arr) {
-    return arr.find(ele => ele.age % 2 === 0)
+    return arr.find(age => age.age % 2 === 0)
 }
 
 
 function includesEvenNum(arr) {
-    return arr.includes((ele) => ele % 2 == 0)
+    return arr.include((ele) => ele % 2 === 0)
 
 }
 
 function includesEvenAge(arr) {
-    return arr.include((a) => a.age % 2 == 0)
+    return arr.includes(age => age.age % 2 === 0)
 
 }
+
+function includesEvenAge(arr) {
+    let even = arr.map(user => user.age % 2 === 0 ? true : false);
+
+    return even.includes(true);
+}
+
+//reduce part
+function sumOfNumbers(arr) {
+
+    return arr.reduce((tot, age) => tot + age, 0)
+
+}
+
+function averageOfNumbers(arr) {
+    let x = arr.reduce((tot, num) => tot + num, 0)
+    return x / arr.length
+
+}
+
+
+
+function maxOfNumbers(arr) {
+
+    return arr.reduce((a, b) => a > b ? a : b)
+
+}
+
+
+function maxOfAges(arr) {
+    return arr.reduce((a, b) => a.age > b.age ? a : b)
+
+
+}
+
+
+function avgAgeEvenNum(arr) {
+    let ages = arr.filter(age => age.age % 2 === 0)
+
+    let chained = arr.map(age => age.age)
+        .filter(age => age % 2 === 0)
+        .reduce((tot, age) => tot + age, 0)
+    return chained / ages.length//86/2
+}
+
+
+function avgAgeoddNum(arr) {
+    let oddages = arr.filter(age => age.age % 2 !== 0)
+
+    let chained = arr.map(age => age.age)
+        .filter(age => age % 2 !== 0)
+        .reduce((tot, age) => tot + age, 0)
+    // console.log(oddages)//[ { name: 'William', age: 6 }, { name: 'Barney', age: 80 } ]
+    return chained / oddages.length//28/2=14
+
+}
+// let test44 = avgAgeoddNum(peopleArray)
+// console.log(test44)
--- CS303/labW1D6/arrayPracticeTests.js
+++ CS303/labW1D6/arrayPracticeTests.js
@@ -1,15 +1,21 @@
 "use strict";
 
-/* comment out the node specific code when going to the browser
-const assert = require("assert");  //always need this with node
-const myExports = require("./arrayPractice.js");  //with node need the name of your file with your functions here
-const doubleNums = myExports.doubleNums;  //do this for all of the functions used in the Mocha tests
-const doubleAges = myExports.doubleAges;
-const filterEven = myExports.filterEven;
-const filterOver10 = myExports.filterOver10;
-const findEvenNum = myExports.findEvenNum;
-const findEvenAge = myExports.findEvenAge;  
-*/
+/* comment out the node specific code when going to the browser*/
+// const assert = require("assert");  //always need this with node
+// const myExports = require("./arrayPractice.js");  //with node need the name of your file with your functions here
+// const doubleNums = myExports.doubleNums;  //do this for all of the functions used in the Mocha tests
+// const doubleAges = myExports.doubleAges;
+// const filterEven = myExports.filterEven;
+// const filterOver10 = myExports.filterOver10;
+// const findEvenNum = myExports.findEvenNum;
+// const findEvenAge = myExports.findEvenAge;
+// const sumOfNumbers = myExports.sumOfNumbers;
+// const averageOfNumbers = myExports.averageOfNumbers;
+// const maxOfNumbers = myExports.maxOfNumbers;
+// const maxOfAges = myExports.maxOfAges;
+// const avgAgeEvenNum = myExports.avgAgeEvenNum;
+// const avgAgeoddNum = myExports.avgAgeoddNum;
+// const includesEvenAge = myExports.includesEvenAge
 
 let numArray;
 let peopleArray;
@@ -25,14 +31,10 @@ describe("map practice", function () {
         assert.deepEqual(numArray, [5, 0, 7, 77, -20, 300, 51, 2]);  //test for pure function
     });
 
-    it("doubles age", function () {
-        assert.deepEqual(doubleAges(peopleArray),
-            [{ name: "Sam", age: 30 }, { name: "William", age: 12 }, { name: "Lucy", age: 26 }, { name: "Barney", age: 160 }]);
-        assert.deepEqual(peopleArray,
-            [{ name: "Sam", age: 15 }, { name: "William", age: 6 }, { name: "Lucy", age: 13 }, { name: "Barney", age: 80 }]);
+    it(" it doubles age", function () {
+        assert.deepEqual(doubleAges(peopleArray), ['30', '12', '26', '160']);
     });
 });
-
 describe("filter practice", function () {
     beforeEach(function () {
         numArray = [5, 0, 7, 77, -20, 300, 51, 2];
@@ -56,7 +58,7 @@ describe("filter practice", function () {
         assert.strictEqual(findEvenNum([1, 3, 801]), undefined);
     });
     it("find even age ", function () {
-        assert.deepEqual(findEvenAge(peopleArray), { name: "William", age: 6 });
+        assert.deepEqual(findEvenAge(peopleArray), { name: 'William', age: 6 });
         const peopleOddAge = [{ name: "Sam", age: 15 }, { name: "Lucy", age: 13 }, { name: "Barney", age: 81 }];
         assert.deepEqual(findEvenAge(peopleOddAge), undefined);
     });
@@ -74,9 +76,45 @@ describe("filter practice", function () {
         assert.strictEqual(numArray.includes(100), false);
         assert.strictEqual(numArray.includes(300), true);
     });
-    it("find even age ", function () {
-        // assert.deepEqual(includesEvenAge(peopleArray),[{ name: "William", age: 6 },{ name: "Barney", age: 80 }])//{ name: "Barney", age: 80 }]);
-        const peopleOddAge = [{ name: "Sam", age: 13 }, { name: "Lucy", age: 13 }, { name: "Barney", age: 81 }];
-        assert.deepEqual(includesEvenAge.includes({ name: "Sam", age: 10 }), true);
+
+    it("includes even ages", function () {
+        assert.strictEqual(includesEvenAge(peopleArray), true);
+        const peopleOddAge = [{ name: "Sam", age: 15 }, { name: "Lucy", age: 13 }, { name: "Barney", age: 81 }];
+        assert.strictEqual(includesEvenAge(peopleOddAge), false);
     });
+
+
 });
+describe("reduce practice", function () {
+    beforeEach(function () {
+        numArray = [5, 0, 7, 77, -20, 300, 51, 2];
+        peopleArray = [{ name: "Sam", age: 15 }, { name: "William", age: 6 }, { name: "Lucy", age: 13 }, { name: "Barney", age: 80 }];
+    });
+    it("find sum of numbers", function () {
+        assert.strictEqual(sumOfNumbers(numArray), 422);
+    });
+
+
+    it("find average of numbers", function () {
+        assert.strictEqual(averageOfNumbers(numArray), 52.75);
+    });
+
+    it("find max of numbers", function () {
+        assert.strictEqual(maxOfNumbers(numArray), 300);
+    });
+
+    it("find max for ages", function () {
+        assert.deepEqual(maxOfAges(peopleArray), { name: 'Barney', age: 80 });
+    });
+
+    it("find max for ages", function () {
+        assert.deepEqual(avgAgeEvenNum(peopleArray), 43);
+    });
+    it("find max for ages", function () {
+        assert.deepEqual(avgAgeoddNum(peopleArray), 14);
+    });
+})
+
+
+
+
