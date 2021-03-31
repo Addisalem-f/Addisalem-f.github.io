0bc5a34695a8a508bab11a1bb1b7b026a51be92e
Addisalem <addisalemdes@gmail.com>
2021-03-30 21:18:04 -0500

    w2d1Assignment

---
+++ CS303/w2d1Assignment/myArrayTester.js
@@ -0,0 +1,26 @@
+
+const assert = require("assert");
+const myExports = require("./myArray.js");
+// let numArray;
+const myMap  = myExports.myMap;
+const oddValue = myExports.oddValue;
+describe("higher oder practice", function () {
+    // beforeEach(function () {
+    //     numArray = [1, 2, 3, 4, 5];
+    // })
+    it("multiply each element", function () {
+        assert.strictEqual(myMap([1, 2, 3, 4, 5]), [1, 4, 9, 16, 25])
+
+    })
+
+    it("it filters odd elements", function () {
+
+        assert.strictEqual(myFilter([1, 2, 3, 4, 5]), [1, 3, 5])
+    })
+
+    it("it sums values", function () {
+
+        assert.strictEqual(myreduce([1, 2, 3, 4, 5]), 15)
+    })
+
+})
